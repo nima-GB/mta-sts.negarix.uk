8ca19c2b11b82d0d60fbbd2fb5379385517ab910
nima-GB <nima@negarix.uk>
2026-05-21 16:59:32 +0100

    Update login.js

--- api/login.js
+++ api/login.js
@@ -1,1 +1,13 @@
+export default function handler(req, res) {
+    if (req.method !== 'POST') {
+        return res.status(405).json({ message: 'Method not allowed' });
+    }
 
+    const { key } = req.body;
+
+    if (key === process.env.SECRET_KEY) {
+        return res.status(200).json({ authorized: true });
+    } else {
+        return res.status(401).json({ authorized: false });
+    }
+}
